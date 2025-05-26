60d9d81aad3dd2489120b9358da2656a035dd4a1
rita <rita953136@gmail.com>
2025-05-26 17:04:54 +0800

    新增轉盤項目

--- schoolApp2/app/src/main/java/fcu/app/schoolApp/PrizeListAdapter.java
+++ schoolApp2/app/src/main/java/fcu/app/schoolApp/PrizeListAdapter.java
@@ -1,8 +1,11 @@
+
 package fcu.app.schoolApp;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -11,39 +14,53 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class PrizeListAdapter extends RecyclerView.Adapter<PrizeListAdapter.PrizeViewHolder> {
+public class PrizeListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int VIEW_TYPE_NORMAL = 0;
+    private static final int VIEW_TYPE_INPUT = 1;
 
     private List<String> prizeList;
 
     public PrizeListAdapter(List<String> prizeList) {
         this.prizeList = prizeList;
     }
-    private static final int VIEW_TYPE_NORMAL = 0;
-    private static final int VIEW_TYPE_INPUT = 1;
-
-    @NonNull
-    @Override
-    public PrizeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_prize, parent, false);
-        return new PrizeViewHolder(view);
-    }
 
     @Override
-    public void onBindViewHolder(@NonNull PrizeViewHolder holder, int position) {
-        String prize = prizeList.get(position);
-        holder.textPrize.setText(prize);
-
-        holder.btnDelete.setOnClickListener(v -> {
-            prizeList.remove(position);
-            notifyItemRemoved(position);
-            notifyItemRangeChanged(position, prizeList.size());
-        });
+    public int getItemViewType(int position) {
+        return (position == prizeList.size()) ? VIEW_TYPE_INPUT : VIEW_TYPE_NORMAL;
     }
 
     @Override
     public int getItemCount() {
-        return prizeList.size();
+        return prizeList.size() + 1; // +1 for input
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_INPUT) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_prize_input, parent, false);
+            return new InputViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_prize, parent, false);
+            return new PrizeViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof PrizeViewHolder) {
+            String prize = prizeList.get(position);
+            ((PrizeViewHolder) holder).textPrize.setText(prize);
+
+            ((PrizeViewHolder) holder).btnDelete.setOnClickListener(v -> {
+                prizeList.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, prizeList.size());
+            });
+        }
     }
 
     static class PrizeViewHolder extends RecyclerView.ViewHolder {
@@ -56,9 +73,24 @@ public class PrizeListAdapter extends RecyclerView.Adapter<PrizeListAdapter.Priz
             btnDelete = itemView.findViewById(R.id.btnDelete);
         }
     }
-    @Override
-    public int getItemViewType(int position) {
-        return (position == prizeList.size()) ? VIEW_TYPE_INPUT : VIEW_TYPE_NORMAL;
-    }
 
+    class InputViewHolder extends RecyclerView.ViewHolder {
+        EditText editText;
+        Button btnAdd;
+
+        public InputViewHolder(@NonNull View itemView) {
+            super(itemView);
+            editText = itemView.findViewById(R.id.editTextNewPrize);
+            btnAdd = itemView.findViewById(R.id.btnConfirmAdd);
+
+            btnAdd.setOnClickListener(v -> {
+                String newItem = editText.getText().toString().trim();
+                if (!newItem.isEmpty()) {
+                    prizeList.add(newItem);
+                    notifyItemInserted(prizeList.size() - 1);
+                    editText.setText("");
+                }
+            });
+        }
+    }
 }
